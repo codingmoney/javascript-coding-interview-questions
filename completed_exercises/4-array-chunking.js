e30c7d56f84a58cce08b6e588cecb1314c348192
Coding Money <codingmoney@mukhtars-mbp.attlocal.net>
2022-08-23 22:21:36 -0700

    array-chunking

--- completed_exercises/4-array-chunking.js
+++ completed_exercises/4-array-chunking.js
@@ -8,4 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = []
+  let index = 0
+  while(index<array.length){
+    result.push(array.slice(index,index+size))
+    index += size
+  }
+  return result; 
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+//                 0, 1, 2, 3, 4
