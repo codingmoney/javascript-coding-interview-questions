513c4f1cd5d2a17fc52a439608a9d9e8bd600a71
Coding Money <codingmoney@mukhtars-mbp.attlocal.net>
2022-08-25 22:01:13 -0700

    5-titlecase exercise

---
+++ exercises/5-titlecase.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('this is mukhtar from coding money') --> 'This Is Mukhtar From Coding Money'
+//   capitalize('what is titlecase?') --> 'What Is Titlecase?'
+//   capitalize('titles of books, movies, songs, plays and other works') --> 'Titles Of Books, Movies, Songs, Plays And Other Works'
+
+function capitalize(str) {}
+
+console.log(capitalize('this is mukhtar from coding money'));
