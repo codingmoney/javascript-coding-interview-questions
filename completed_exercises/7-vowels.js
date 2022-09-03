7516701c5947045311075e5a032d3571c6cfcac3
Coding Money <codingmoney@mukhtars-mbp.attlocal.net>
2022-09-02 20:31:04 -0700

    vowels

---
+++ completed_exercises/7-vowels.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('How are you?') --> 5
+//   vowels('Coding Money') --> 4
+//   vowels('why?') --> 0 
+
+function vowels(str) {
+ const vowelCheck = ['a', 'e','i', 'o', 'u']
+
+ let count = 0
+
+ for(let char of str){
+  if(vowelCheck.includes(char)) count++
+ }
+
+ return count
+}
+
+console.log(vowels('Coding Money'));
+
+// function vowels(str) {
+//   const matches = str.match(/[aeiou]/gi)
+//   return matches ? matches.length : 0
+//  }
---
+++ exercises/7-vowels.js
@@ -0,0 +1,13 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('How are you?') --> 5
+//   vowels('Coding Money') --> 4
+//   vowels('why?') --> 0 
+
+function vowels(str) {}
+
+console.log(vowels('Coding Money'));
